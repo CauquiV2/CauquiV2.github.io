caf51de24956295d238a8bfa1f0510b162f0338a
Alvaro Cauqui Diaz <alvaro.cauqui@sorevan.com>
2024-07-12 09:33:03 +0200

    updtate

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function statuss(value) {
     if (value <= 40) {
         return 'available';
     }
-    if ((value > 40) && (value < 50)) {
+    if ((value > 40) && (value <= 50)) {
         return 'maintenance';
     }
     if (value > 50) {
@@ -13,9 +13,19 @@ function statuss(value) {
     }
 }
 
+function statusmic(value){
+    if (value>=1){
+        return 'occupied';
+    }
+    else{
+        return 'transparent';
+    }
+}
+
 function updateRoomStatus(inputElement) {
     const value = parseInt(inputElement.value, 10);
-    const status = statuss(value);
+    const group = inputElement.dataset.group;
+    const status = group === 'room' ? statuss(value) : statusmic(value);
     const roomId = inputElement.dataset.roomId;
     const color = {
         'available': 'green',
@@ -86,30 +96,30 @@ document.addEventListener('DOMContentLoaded', function() {
         const naturalHeight = 2339; // Replace with your image's natural height
 
         const rooms = [
-            { id: 'room1', coords: '385,584,635,752', shape: 'rect', status: 'available' },
-            { id: 'room2', coords: '733,585,1012,751', shape: 'rect', status: 'occupied' },
-            { id: 'room3', coords: '496,262,681,349', shape: 'rect', status: 'maintenance' },
-            { id: 'room4', coords: '683,260,848,348', shape: 'rect', status: 'available' },
-            { id: 'room5', coords: '500,816,664,1015', shape: 'rect', status: 'occupied' },
-            { id: 'room6', coords: '354,814,495,1012', shape: 'rect', status: 'maintenance' },
-            { id: 'room7', coords: '363,450,465,577', shape: 'rect', status: 'available' },
-            { id: 'room8', coords: '473,449,589,576', shape: 'rect', status: 'occupied' },
-            { id: 'room9', coords: '597,444,712,578', shape: 'rect', status: 'maintenance' },
-            { id: 'room10', coords: '714,450,832,577', shape: 'rect', status: 'available' },
-            { id: 'room11', coords: '667,812,886,812,886,939,775,945,771,1014,670,1014', shape: 'poly', status: 'occupied' },
-            { id: 'room12', coords: '1064,818,1062,1015,1406,1010,1407,815', shape: 'poly', status: 'maintenance' },
-            { id: 'room13', coords: '267,404,321,654', shape: 'rect', status: 'available' },
-            { id: 'room14', coords: '267,657,321,796', shape: 'rect', status: 'occupied' },
-            { id: 'room4m1', coords: '754,604,14', shape: 'circle', status: 'occupied' },
-            { id: 'room4m2', coords: '1003,737,14', shape: 'circle', status: 'occupied' },
-            { id: 'room4m3', coords: '998,597,14', shape: 'circle', status: 'occupied' },
-            { id: 'room4m4', coords: '749,736,17', shape: 'circle', status: 'occupied' },
-            { id: 'room3m1', coords: '461,594,10', shape: 'circle', status: 'occupied' },
-            { id: 'room3m2', coords: '560,591,13', shape: 'circle', status: 'occupied' },
-            { id: 'room3m3', coords: '625,642,12', shape: 'circle', status: 'occupied' },
-            { id: 'room3m4', coords: '625,708,12', shape: 'circle', status: 'occupied' },
-            { id: 'room3m5', coords: '557,740,10', shape: 'circle', status: 'occupied' },
-            { id: 'room3m6', coords: '467,739,10', shape: 'circle', status: 'occupied' },
+            { id: 'room1', coords: '385,584,635,752', shape: 'rect',group:'room', status: 'available' },
+            { id: 'room2', coords: '733,585,1012,751', shape: 'rect',group:'room', status: 'occupied' },
+            { id: 'room3', coords: '496,262,681,349', shape: 'rect', group:'room',status: 'maintenance' },
+            { id: 'room4', coords: '683,260,848,348', shape: 'rect', group:'room',status: 'available' },
+            { id: 'room5', coords: '500,816,664,1015', shape: 'rect', group:'room',status: 'occupied' },
+            { id: 'room6', coords: '354,814,495,1012', shape: 'rect', group:'room',status: 'maintenance' },
+            { id: 'room7', coords: '363,450,465,577', shape: 'rect', group:'room',status: 'available' },
+            { id: 'room8', coords: '473,449,589,576', shape: 'rect', group:'room',status: 'occupied' },
+            { id: 'room9', coords: '597,444,712,578', shape: 'rect', group:'room',status: 'maintenance' },
+            { id: 'room10', coords: '714,450,832,577', shape: 'rect', group:'room',status: 'available' },
+            { id: 'room11', coords: '667,812,886,812,886,939,775,945,771,1014,670,1014', shape: 'poly', group:'room', status: 'occupied' },
+            { id: 'room12', coords: '1064,818,1062,1015,1406,1010,1407,815', shape: 'poly', group:'room',status: 'maintenance' },
+            { id: 'room13', coords: '267,404,321,654', shape: 'rect', group:'room',status: 'available' },
+            { id: 'room14', coords: '267,657,321,796', shape: 'rect', group:'room',status: 'occupied' },
+            { id: 'room4m1', coords: '754,604,14', shape: 'circle', group:'mic',status: 'occupied' },
+            { id: 'room4m2', coords: '1003,737,14', shape: 'circle', group:'mic',status: 'occupied' },
+            { id: 'room4m3', coords: '998,597,14', shape: 'circle', group:'mic',status: 'occupied' },
+            { id: 'room4m4', coords: '749,736,17', shape: 'circle', group:'mic',status: 'occupied' },
+            { id: 'room3m1', coords: '461,594,10', shape: 'circle',  group:'mic',status: 'occupied' },
+            { id: 'room3m2', coords: '560,591,13', shape: 'circle',  group:'mic',status: 'occupied' },
+            { id: 'room3m3', coords: '625,642,12', shape: 'circle',  group:'mic',status: 'occupied' },
+            { id: 'room3m4', coords: '625,708,12', shape: 'circle',  group:'mic',status: 'occupied' },
+            { id: 'room3m5', coords: '557,740,10', shape: 'circle',  group:'mic',status: 'occupied' },
+            { id: 'room3m6', coords: '467,739,10', shape: 'circle',  group:'mic',status: 'occupied' },
         ];
 
         const convertToPercentage = (pixelValue, dimension) => (pixelValue / dimension) * 100;
